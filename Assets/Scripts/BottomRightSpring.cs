22231a176fce54dc0b86976a1e653ec6f38a268d
Tim <tim.de.haas@hotmail.nl>
2022-04-25 20:25:11 +0200

    refactor 4

--- Assets/Scripts/BottomRightSpring.cs
+++ Assets/Scripts/BottomRightSpring.cs
@@ -4,7 +4,7 @@ public class BottomRightSpring : MonoBehaviour
 {
     public GameObject bottom_spring;
 
-    public float speed;
+    public float velocity;
     public float mass = 1.0f;
     public float spring_constant_top = 1.0f;
     public float rest_length_top = 5.0f;
@@ -15,10 +15,8 @@ public class BottomRightSpring : MonoBehaviour
     {
         float delta = (bottom_spring.transform.position.x + rest_length_top) - transform.position.x;
         float force = spring_constant_top * delta;
-        float velocity = speed + force / mass * Time.deltaTime;
+        velocity += force / mass * Time.deltaTime;
         
         transform.position = new Vector3(transform.position.x + velocity * Time.deltaTime, transform.position.y, transform.position.z);
-        
-        speed = velocity;
     }
 }
--- Assets/Scripts/BounceBallTopRight.cs
+++ Assets/Scripts/BounceBallTopRight.cs
@@ -9,7 +9,7 @@ public class BounceBallTopRight : MonoBehaviour
     private Bounds _ballBounds;
     private Bounds _bottomBallBounds;
         
-    public float speed = 0F;
+    public float speed;
     public float mass = 1.0f;
 
     void Start()
@@ -25,29 +25,29 @@ public class BounceBallTopRight : MonoBehaviour
         // get location bottom_ball
         // float bottom_ball_x = bottomBall.transform.position.x;
 
-        float new_speed = speed - 9.81f * Time.deltaTime;
-        float new_position = transform.position.y + new_speed * Time.deltaTime;
+        float velocity = speed - 9.81f * Time.deltaTime;
+        float pos = transform.position.y + velocity * Time.deltaTime;
 
-        if (new_position + _ballBounds.size.y / 2 > _areaBounds.max.y)
+        if (pos + _ballBounds.size.y / 2 > _areaBounds.max.y)
         {
-            new_position = _areaBounds.max.y - _ballBounds.size.y / 2;
-            new_speed = -new_speed;
+            pos = _areaBounds.max.y - _ballBounds.size.y / 2;
+            velocity = -velocity;
         }
 
-        if (new_position - _ballBounds.size.y / 2 < bottomBall.transform.position.y + _bottomBallBounds.size.y / 2)
+        if (pos - _ballBounds.size.y / 2 < bottomBall.transform.position.y + _bottomBallBounds.size.y / 2)
         {
             float bottom_ball_mass = bottomBall.GetComponent<BounceBallBottomRight>().mass;
             float bottom_ball_speed = bottomBall.GetComponent<BounceBallBottomRight>().speed;
 
-            new_position = bottomBall.transform.position.y + _bottomBallBounds.size.y / 2 + _ballBounds.size.y / 2;
+            pos = bottomBall.transform.position.y + _bottomBallBounds.size.y / 2 + _ballBounds.size.y / 2;
 
-            float updated_speed = (bottom_ball_mass * bottom_ball_speed * (1 + 1) + new_speed * (mass - bottom_ball_mass * 1))/(mass + bottom_ball_mass);
-            bottomBall.GetComponent<BounceBallBottomRight>().speed = (mass * new_speed * (1 + 1) + bottom_ball_speed * (bottom_ball_mass - mass * 1))/(mass + bottom_ball_mass);
+            float updated_speed = (bottom_ball_mass * bottom_ball_speed * (1 + 1) + velocity * (mass - bottom_ball_mass * 1))/(mass + bottom_ball_mass);
+            bottomBall.GetComponent<BounceBallBottomRight>().speed = (mass * velocity * (1 + 1) + bottom_ball_speed * (bottom_ball_mass - mass * 1))/(mass + bottom_ball_mass);
             
-            new_speed = updated_speed;
+            velocity = updated_speed;
         }
 
-        transform.position = new Vector3(transform.position.x, new_position, transform.position.z);
-        speed = new_speed;
+        transform.position = new Vector3(transform.position.x, pos, transform.position.z);
+        speed = velocity;
     }
 }
